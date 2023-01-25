51624d8ecf74f6387af9c43c841bc6eacbc1105c
Marcel <marcelstenkewitz@gmail.com>
2023-01-25 11:34:28 -0500

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Search from "./Search";
+import ThemeIcon from "./ThemeIcon";
+
+const Header = ({ name }) => {
+  return (
+    <>
+      <div className="xl:px-32">
+        <h1 className="text-5xl pb-2">{name}</h1>
+        <ThemeIcon/>
+        <Search />
+      </div>``
+    </>
+  );
+};
+
+export default Header;
